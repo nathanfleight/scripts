41f6a787d8e1687cc00bbaba23d5eca0640aab61
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2021-12-21 19:52:11 +0200

    Create install_sockd.sh

---
+++ install_sockd.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+apt update;apt -y install gcc cmake binutils build-essential net-tools
+ifaceName=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
+wget https://www.inet.no/dante/files/dante-1.4.3.tar.gz
+tar -xvzf dante-1.4.3.tar.gz
+cd dante-1.4.3
+./configure
+make
+make install
+
+cat > /etc/sockd.conf <<EOF
+logoutput: /var/log/socks.log
+
+internal: $ifaceName port = 1080
+external: $ifaceName
+
+socksmethod: username
+
+user.unprivileged: nobody
+user.privileged: root
+
+client pass {
+from: 0.0.0.0/0 to: 0.0.0.0/0
+log: error
+}
+
+socks pass {
+from: 0.0.0.0/0 to: 0.0.0.0/0
+command: connect
+log: error
+socksmethod: username
+protocol: tcp udp
+}
+EOF
+
+/usr/local/sbin/sockd -D &
+
+useradd --shell /usr/sbin/nologin mikrotik999
+echo "mikrotik999:Elibawnos" | chpasswd
+
+netstat -ntlp

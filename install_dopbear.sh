5f9b4f47bcc9d7b05d12095384ae88a78e928484
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2021-12-21 21:27:04 +0200

    Update install_dopbear.sh

--- install_dopbear.sh
+++ install_dopbear.sh
@@ -1,3 +1,5 @@
 apt update;apt -y install dropbear
 sed -i "s/NO_START=1/NO_START=0/" /etc/default/dropbear
 sed -i "s/DROPBEAR_PORT=22/DROPBEAR_PORT=443/" /etc/default/dropbear
+service dropbear restart
+netstat -ntlp

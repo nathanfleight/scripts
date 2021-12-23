b64d18f099868993fe80559369bde81ff92f7153
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2021-12-23 10:56:34 +0200

    Update set_up_aws_aio.sh

--- set_up_aws_aio.sh
+++ set_up_aws_aio.sh
@@ -3,3 +3,5 @@ wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/install_s
 wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/install_dopbear.sh | bash
 wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/install_shadowsocks.sh | bash
 wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/prep_aws_root_login.sh | bash
+netstat -ntlp
+apt -y upgrade;reboot

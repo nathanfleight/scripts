21f344159619c9b43d98ff7004a10b538cbb1476
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-02-04 14:36:04 +0200

    Create install_fail2ban.sh

---
+++ install_fail2ban.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+apt update;apt -y install fail2ban
+systemctl enable fail2ban.service
+
+cat > /etc/fail2ban/jail.local <<EOL
+[sshd]
+enabled = true
+port = ssh
+filter = sshd
+logpath = /var/log/auth.log
+maxretry = 3
+bantime = -1
+ignoreip = 127.0.0.1
+EOL
+
+
+systemctl restart fail2ban.service
+systemctl status fail2ban.service

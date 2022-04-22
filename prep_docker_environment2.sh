ef7f806748d1aae6efd8f41371485624fac73bb5
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-04-22 18:51:28 +0200

    Create prep_docker_environment2.sh

---
+++ prep_docker_environment2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mkdir ~/docker
+cd ~/docker
+
+echo "FROM nvidia/cuda:11.0-cudnn8-devel-ubuntu18.04" > Dockerfile
+echo "CMD [\"/bin/bash\"]" >> Dockerfile
+
+docker build --tag mirekphd/docker .
+
+docker run --rm -it mirekphd/docker bash
+cd root
+apt update;apt -y install wget net-tools nano

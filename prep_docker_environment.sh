f75ad777a8464e22fbd35c60924c9b624506b1ec
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-04-22 18:54:03 +0200

    Update prep_docker_environment.sh

--- prep_docker_environment.sh
+++ prep_docker_environment.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-apt update;apt -y install screen docker.io;screen -S Test
-mkdir ~/docker
-cd ~/docker
-
-echo "FROM nvidia/cuda:11.0-cudnn8-devel-ubuntu18.04" > Dockerfile
-echo "CMD [\"/bin/bash\"]" >> Dockerfile
-
-docker build --tag mirekphd/docker .
-
-docker run --rm -it mirekphd/docker bash
-cd root
-apt update;apt -y install wget net-tools nano screen
+apt update;apt -y install screen docker.io
+screen -dmS Laboratory bash -c 'apt update;apt -y install wget;wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/prep_docker_environment2.sh | bash; exec bash'

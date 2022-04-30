f64d2208bed23e64049d8031c4c201a8c88f6765
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-04-30 21:02:10 +0200

    Create prep_2004_repos.sh

---
+++ prep_2004_repos.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+rm /etc/apt/sources.list.d/*
+rm /etc/apt/sources.list
+
+cat > /etc/apt/sources.list <<EOL
+deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+
+deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+
+deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+
+deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+
+deb http://archive.canonical.com/ubuntu focal partner
+deb-src http://archive.canonical.com/ubuntu focal partner
+EOL
+
+apt update;apt -y upgrade

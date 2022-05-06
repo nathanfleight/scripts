08ab72f6bbe3d0091293b3531c1870fa0ab9c0a7
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-05-06 02:22:53 +0200

    Update prep_codeserver.sh

--- prep_codeserver.sh
+++ prep_codeserver.sh
@@ -7,11 +7,12 @@ apt -y install nodejs
 
 npm install -g localtunnel
 
+code-server &
 
-code-server --bind-addr 127.0.0.1:8040 & lt --port 8040 &
 ls -la
 cat ~/.config/code-server/config.yaml
 
 cd ~/.config/code-server
 cat config.yaml
 
+code-server --bind-addr 127.0.0.1:8040 & lt --port 8040

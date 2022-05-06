2840f689788324299f9e6e66b2432a2d19aced66
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-05-06 02:14:41 +0200

    Update prep_codeserver.sh

--- prep_codeserver.sh
+++ prep_codeserver.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
 curl -fsSL https://code-server.dev/install.sh | sh
-ls -la
-find / -name config.yaml
 
 wget -O - https://deb.nodesource.com/setup_17.x | bash
 apt -y install nodejs
 
 npm install -g localtunnel
+cat ~/.config/code-server/config.yaml
+
+cd ~/.config/code-server
+cat config.yaml
+
 code-server --bind-addr 127.0.0.1:8040 & lt --port 8040
 

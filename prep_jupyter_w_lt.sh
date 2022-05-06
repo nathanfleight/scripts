db3dd1a9d8e5b67a61f81a38186608ee7cd93e01
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-05-06 15:56:21 +0200

    Create prep_jupyter_w_lt.sh

---
+++ prep_jupyter_w_lt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+pip install jupyter
+wget -O - https://deb.nodesource.com/setup_17.x | bash
+apt -y install nodejs
+
+npm install -g localtunnel
+
+jupyter notebook --port 1234 --allow-root & lt --port 1234

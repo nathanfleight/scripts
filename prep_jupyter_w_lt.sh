60bc244a62c07af26b3c1c1cc4f30b9d509453cd
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-05-23 22:45:13 +0200

    Update prep_jupyter_w_lt.sh

--- prep_jupyter_w_lt.sh
+++ prep_jupyter_w_lt.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-pip install jupyter
+
+#pip install jupyter
 wget -O - https://deb.nodesource.com/setup_17.x | bash
 apt -y install nodejs
 
@@ -7,7 +8,11 @@ npm install -g localtunnel
 
 #find / -name 'jupyter_notebook_config.py' -print
 find ~/ -name 'jupyter_notebook_config.py' -print
-#find . -name 'jupyter_notebook_config.py' -print
+
+cat >> ~/.jupyter/jupyter_notebook_config.py <<EOL
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.ip = '0.0.0.0'
+EOL
 
 ls -la
 

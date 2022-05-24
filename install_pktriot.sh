ba4ad8803e74563ca2706e6b656529d470a21892
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-05-24 02:01:46 +0200

    Create install_pktriot.sh

---
+++ install_pktriot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt-get install apt-transport-https gnupg -y
+
+wget -qO - https://download.packetriot.com/linux/debian/pubkey.gpg |  apt-key add - 
+ 
+echo "
+deb [arch=amd64] https://download.packetriot.com/linux/debian/buster/stable/non-free/binary-amd64 / 
+deb [arch=i386]  https://download.packetriot.com/linux/debian/buster/stable/non-free/binary-i386  / 
+deb [arch=armhf] https://download.packetriot.com/linux/debian/buster/stable/non-free/binary-armhf / 
+deb [arch=arm64] https://download.packetriot.com/linux/debian/buster/stable/non-free/binary-arm64 / 
+" |  tee /etc/apt/sources.list.d/packetriot.list
+
+apt update;apt -y install pktriot

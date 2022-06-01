520e1832995c9849aca0973598895a90f15f96a7
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-06-01 23:43:02 +0200

    Update prep_oktet_w_ssh.sh

--- prep_oktet_w_ssh.sh
+++ prep_oktet_w_ssh.sh
@@ -13,5 +13,5 @@ netstat -ntlp
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok authtoken 29zaCvGiC7z56OHlOTiDALBZigi_7rqDA8efFY5FqvNECuGri
-./ngrok tcp 22 && curl http://127.0.0.1:4040/api/tunnels &
+./ngrok tcp 22 & curl http://127.0.0.1:4040/api/tunnels
 netstat -ntlp

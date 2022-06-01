0db760a674924fcfa4f358dbe8f6af7d77e72652
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-06-01 22:37:58 +0200

    Update prep_codeserver.sh

--- prep_codeserver.sh
+++ prep_codeserver.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget -O - https://deb.nodesource.com/setup_17.x | bash
 apt -y install nodejs
+apt -y install npm
 npm install -g localtunnel
 
 wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-amd64.tar.gz

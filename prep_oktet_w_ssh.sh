8e14eef552adafe75212a1c16e23a4c5b7d5c520
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-06-01 23:46:03 +0200

    Update prep_oktet_w_ssh.sh

--- prep_oktet_w_ssh.sh
+++ prep_oktet_w_ssh.sh
@@ -4,6 +4,7 @@ service ssh restart
 
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
 sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sed -i "s/#Port 22/Port 22/" /etc/ssh/sshd_config
 service ssh restart
 echo "root:Pmataga87465622" | chpasswd
 
@@ -13,5 +14,4 @@ netstat -ntlp
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok authtoken 29zaCvGiC7z56OHlOTiDALBZigi_7rqDA8efFY5FqvNECuGri
-./ngrok tcp 22 & curl http://127.0.0.1:4040/api/tunnels
-netstat -ntlp
+./ngrok tcp 22 

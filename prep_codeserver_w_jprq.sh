36f7d44b7223eacacd8e44f825b8da423a055435
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-06-05 00:19:17 +0200

    Update prep_codeserver_w_jprq.sh

--- prep_codeserver_w_jprq.sh
+++ prep_codeserver_w_jprq.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pip3 install jprq
 wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-amd64.tar.gz
-tar -xvf code-server-4.4.0-linux-amd64.tar.gz
+tar -xf code-server-4.4.0-linux-amd64.tar.gz > /dev/null
 cd code-server-4.4.0-linux-amd64
 cd bin
 export PASSWORD="Elibawnos"

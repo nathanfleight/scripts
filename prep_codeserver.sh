e52ed1b7ea2bf323c277c7a299b87325ae2aca1b
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-06-08 12:58:28 +0200

    Update prep_codeserver.sh

--- prep_codeserver.sh
+++ prep_codeserver.sh
@@ -5,7 +5,7 @@ apt -y install npm
 npm install -g localtunnel
 
 wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-amd64.tar.gz
-tar -xvf code-server-4.4.0-linux-amd64.tar.gz
+tar -xf code-server-4.4.0-linux-amd64.tar.gz
 cd code-server-4.4.0-linux-amd64
 cd bin
 export PASSWORD="Elibawnos"

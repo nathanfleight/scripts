fc7a0798e4ab51cb4ff6dbb74fcc13904de4028b
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-06-28 21:43:59 +0200

    Create prep_jupyter_w_jprq.sh

---
+++ prep_jupyter_w_jprq.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+apt -y install python3 python3-pip >/dev/null
+pip3 install --upgrade pip >/dev/null
+
+
+pip3 install jprq
+
+find / -name 'jupyter_notebook_config.py' -print
+find ~/ -name 'jupyter_notebook_config.py' -print
+
+cat >> ~/.jupyter/jupyter_notebook_config.py <<EOL
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.ip = '0.0.0.0'
+EOL
+
+ls -la
+
+cat ~/.jupyter/jupyter_notebook_config.py
+
+jupyter notebook --port 1234 --allow-root & jprq tcp 1234
